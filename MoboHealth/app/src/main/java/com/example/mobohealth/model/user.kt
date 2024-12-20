1266042b65b8e6412b1fad9d53fa61aa1f65a900
Sarjak <bhandarisarjak@gmail.com>
2024-12-20 19:25:07 +0545

    Splash Screen

---
+++ MoboHealth/app/src/main/java/com/example/mobohealth/model/user.kt
@@ -0,0 +1,7 @@
+package com.example.mobohealth.model
+
+class user {
+    private lateinit var Username: String;
+    private lateinit var Password: String;
+    private lateinit var type: String;
+}
