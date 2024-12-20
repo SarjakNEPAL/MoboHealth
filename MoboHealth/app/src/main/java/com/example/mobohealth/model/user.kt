66fb7de30cdb517a6ed24a81a30df600e7dea9e9
Sarjak Bhandari <bhandarisarjak@gmail.com>
2024-12-20 19:49:33 +0545

    removed all files

--- MoboHealth/app/src/main/java/com/example/mobohealth/model/user.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mobohealth.model
-
-class user {
-    private lateinit var Username: String;
-    private lateinit var Password: String;
-    private lateinit var type: String;
-}
